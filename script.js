097ab187aa7610692a1bd91afd16382f411ee85e
Dimon <barbenyagre.dmytro@gmail.com>
2023-03-04 20:28:49 +0300

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//document.cookie = "Dima"
+//document.cookie = 14
+//
+//document.write(document.cookie)
+
+document.cookie = "name=Dima"
+document.cookie = "age=14"
+document.write(document.cookie)
